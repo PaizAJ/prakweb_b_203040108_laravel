79da8b1fb4574b1d7d490451bed1d88347efac28
John Doe <johndoe@example.com>
2022-11-04 09:40:13 +0700

    #13 Searching & Pagination

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,15 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home" 
+        "title" => "Home",
+        "active" => 'home' 
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => 'about',
         "name" => "Paiz Abdul Jabar",
         "email" => "alafifinuraziz957@gmail.com",
         "image" => "profile.jpeg"
@@ -34,7 +36,7 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/blog', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 
@@ -46,17 +48,4 @@ Route::get('/categories', function(){
     ]);
 });
 
-Route::get('/categories/{category:slug}', function(Category $category) {
-    return view('posts', [
-        'title' => "Post By Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
 
-Route::get('/authors/{author:username}', function(User $author) {
-    return view('posts', [
-        'title' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category', 'author')
-    ]);
-});
