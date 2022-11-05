614d5559f626824d0db6470fb15e0e9592052393
John Doe <johndoe@example.com>
2022-11-05 16:00:01 +0700

    User Registration

--- routes/web.php
+++ routes/web.php
@@ -52,3 +52,4 @@ Route::get('/login', [LoginController::class, 'index']);
 
 
 Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
